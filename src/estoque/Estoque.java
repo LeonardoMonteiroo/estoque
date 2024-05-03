5ae65941c548efe78cc64e0f56f225500ca5ced5
Gabriel-Caldeira <gabrielcaldeiraprestes@gmail.com>
2024-05-03 19:47:36 -0300

    Initial Commit

--- src/estoque/Estoque.java
+++ src/estoque/Estoque.java
@@ -14,7 +14,7 @@ public class Estoque {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("AAAAAAAAAAAAAAAHHHHHHHHHH");
     }
     
 }
