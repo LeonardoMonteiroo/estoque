d36fd5ec09b0f6f6eeab2c50f2496427cd1dfe62
LeonardoMonteiroo <92556142+LeonardoMonteiroo@users.noreply.github.com>
2024-05-03 17:51:06 -0300

    Initial commit

---
+++ src/DAO/ProdutoDAO.java
@@ -0,0 +1,185 @@
+package DAO;
+
+import Model.Produto;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ProdutoDAO {
+
+    public static ArrayList<Produto> produtos = new ArrayList<Produto>();
+
+    //Construtor vazio
+    public ProdutoDAO() {
+    }
+
+    //DriverManager
+    public Connection getConexao() {
+
+        Connection connection = null;
+
+        try {
+
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+
+            String server = "";
+            String database = "";
+            String url = "jdbc:mysql://" + server + ":3306/" + database + "?useTimezone=true&serverTimezone=UTC";
+            String user = "";
+            String password = "";
+
+            connection = DriverManager.getConnection(url, user, password);
+
+            return connection;
+
+        } catch (ClassNotFoundException error) {
+            System.out.println("Status: O driver do banco de dados não foi encontrado. Erro: " + error.getMessage());
+            return null;
+
+        } catch (SQLException error) {
+            System.out.println("Status: Nao foi possível conectar ao banco de dados. Erro: " + error.getMessage());
+            return null;
+        }
+    }
+
+    //Retorna uma ArrayList com os produtos
+    public ArrayList getProdutos() {
+
+        produtos.clear();
+
+        try {
+
+            Statement stmt = this.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_produto");
+            while (res.next()) {
+
+                int id_produto = res.getInt("id_produto");
+                String nome_produto = res.getString("nome_produto");
+                String descricao_produto = res.getString("descricao_produto");
+                int quantidade_estoque = res.getInt("quantidade_estoque");
+                float preco = res.getFloat("preco");
+                int data_cadastro = res.getInt("data_cadastro");
+
+                Produto objeto = new Produto(id_produto, nome_produto, descricao_produto, quantidade_estoque, preco, data_cadastro);
+
+                produtos.add(objeto);
+            }
+
+            stmt.close();
+
+        } catch (SQLException error) {
+            System.out.println("Erro: " + error.getMessage());
+        }
+
+        return produtos;
+    }
+
+    //Retorna o maior ID
+    public int maiorID() throws SQLException {
+
+        int maiorID = 0;
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT MAX(id_produto) id FROM tb_produto");
+            res.next();
+            maiorID = res.getInt("id_produto");
+
+            stmt.close();
+
+        } catch (SQLException error) {
+            System.out.println("Erro: " + error.getMessage());
+        }
+
+        return maiorID;
+    }
+
+    //Cadastra novo produto
+    public boolean InsertProdutoBD(Produto objeto) {
+        String sql = "INSERT INTO tb_produto(id_produto, nome_produto, descricao_produto, quantidade_estoque, preco, data_cadastro) VALUES(?, ?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+            stmt.setInt(1, objeto.getId_produto());
+            stmt.setString(2, objeto.getNome_produto());
+            stmt.setString(3, objeto.getDescricao_produto());
+            stmt.setInt(4, objeto.getQuantidade_estoque());
+            stmt.setFloat(5, objeto.getPreco());
+            stmt.setInt(6, objeto.getData_cadastro());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+
+        } catch (SQLException error) {
+            throw new RuntimeException(error);
+        }
+
+    }
+
+    //Deleta um produto específico pelo seu campo ID
+    public boolean DeleteProdutoBD(int id) {
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            stmt.executeUpdate("DELETE FROM tb_produto WHERE id_produto = " + id);
+            stmt.close();
+
+        } catch (SQLException error) {
+            System.out.println(error.getMessage());
+        }
+
+        return true;
+    }
+
+    //Edita um produto específico pelo seu campo ID
+    public boolean UpdateProdutoBD(Produto objeto) {
+
+        String sql = "UPDATE tb_produto set nome_produto = ?, descricao_produto = ?, quantidade_estoque = ?, preco = ?, data_cadastro = ? WHERE id_produto = ?";
+
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+            stmt.setInt(1, objeto.getId_produto());
+            stmt.setString(2, objeto.getNome_produto());
+            stmt.setString(3, objeto.getDescricao_produto());
+            stmt.setInt(4, objeto.getQuantidade_estoque());
+            stmt.setFloat(5, objeto.getPreco());
+            stmt.setInt(6, objeto.getData_cadastro());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+
+        } catch (SQLException error) {
+            throw new RuntimeException(error);
+        }
+    }
+
+    //Retorna um produto pelo seu campo ID
+    public Produto carregaProduto(int id) {
+
+        Produto objeto = new Produto();
+        objeto.setId_produto(id);
+
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_produto WHERE id_produto = " + id);
+            res.next();
+
+            objeto.setNome_produto(res.getString("nome_produto"));
+            objeto.setDescricao_produto(res.getString("descricao_produto"));
+            objeto.setQuantidade_estoque(res.getInt("quantidade_estoque"));
+            objeto.setPreco(res.getFloat("preco"));
+            objeto.setData_cadastro(res.getInt("data_cadastro"));
+
+            stmt.close();
+
+        } catch (SQLException error) {
+            System.out.println("Erro: " + error.getMessage());
+        }
+        return objeto;
+    }
+}
---
+++ src/Model/Produto.java
@@ -0,0 +1,120 @@
+package Model;
+
+import DAO.ProdutoDAO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Produto {
+
+    private final ProdutoDAO dao;
+    private int id_produto;
+    private String nome_produto;
+    private String descricao_produto;
+    private int quantidade_estoque;
+    private float preco;
+    private int data_cadastro;
+
+    //Construtor vazio
+    public Produto() {
+        this.dao = new ProdutoDAO();
+    }
+
+    //Construtor
+    public Produto(int id_produto, String nome_produto, String descricao_produto, int quantidade_estoque, float preco, int data_cadastro) {
+        this.id_produto = id_produto;
+        this.nome_produto = nome_produto;
+        this.descricao_produto = descricao_produto;
+        this.quantidade_estoque = quantidade_estoque;
+        this.preco = preco;
+        this.data_cadastro = data_cadastro;
+        this.dao = new ProdutoDAO();
+    }
+
+    //Início dos métodos getters e setters
+    public int getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(int id_produto) {
+        this.id_produto = id_produto;
+    }
+
+    public String getNome_produto() {
+        return nome_produto;
+    }
+
+    public void setNome_produto(String nome_produto) {
+        this.nome_produto = nome_produto;
+    }
+
+    public String getDescricao_produto() {
+        return descricao_produto;
+    }
+
+    public void setDescricao_produto(String descricao_produto) {
+        this.descricao_produto = descricao_produto;
+    }
+
+    public int getQuantidade_estoque() {
+        return quantidade_estoque;
+    }
+
+    public void setQuantidade_estoque(int quantidade_estoque) {
+        this.quantidade_estoque = quantidade_estoque;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public int getData_cadastro() {
+        return data_cadastro;
+    }
+
+    public void setData_cadastro(int data_cadastro) {
+        this.data_cadastro = data_cadastro;
+    }
+    //Fim dos métodos getters e setters
+
+    //
+    public ArrayList getProdutos() {
+        return dao.getProdutos();
+    }
+
+    //Retorna o maior ID
+    public int maiorID() throws SQLException {
+        return dao.maiorID();
+    }
+
+    //Cadastra novo produto
+    public boolean InsertProdutoBD(int id_produto, String nome_produto, String descricao_produto, int quantidade_estoque, float preco, int data_cadastro) throws SQLException {
+        int id = this.maiorID() + 1;
+        Produto objeto = new Produto(id_produto, nome_produto, descricao_produto, quantidade_estoque, preco, data_cadastro);
+        dao.InsertProdutoBD(objeto);
+        return true;
+    }
+
+    //Deleta um produto específico pelo seu campo ID
+    public boolean DeleteProdutoBD(int id) {
+        dao.DeleteProdutoBD(id);
+        return true;
+    }
+
+    //Edita um produto específico pelo seu campo ID
+    public boolean UpdateAlunoBD(int id_produto, String nome_produto, String descricao_produto, int quantidade_estoque, float preco, int data_cadastro) {
+        Produto objeto = new Produto(id_produto, nome_produto, descricao_produto, quantidade_estoque, preco, data_cadastro);
+        dao.UpdateProdutoBD(objeto);
+        return true;
+    }
+
+    //Retorna um produto pelo seu campo ID
+    public Produto carregaAluno(int id) {
+        dao.carregaProduto(id);
+        return null;
+    }
+}
